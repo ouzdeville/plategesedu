ce43b2cf888f9d0adbbee0d0116dfbb4140061b7
ouzdeville <ouzdeville@gmail.com>
2020-08-24 14:26:45 +0000

    delete add

--- index.php
+++ index.php
@@ -1,68 +1,71 @@
 <html>
-             <head>
-			 <title>Plateforme ESSA</title>
-			  <h1> BIENVENUE  A  L'ECOLE  SUPERIEURE  DES  SCIENCES  APPLIQUEES   E.S.S.A</h1>
-			 <meta charset="UTF-8"/>
-			 </head>
-			  <body>
-			  <?php
-			  ?>
- <img class="a" src="ESSA.PNG" height="110" width="180"/>
-<img class="b" src="CAMES.PNG" height="110" width="180"/>
-<link rel="stylesheet" href="bootstrap.min.css">
- <link rel="stylesheet" href="essa.css"/>
- <section class="no">
-<ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link" href="#">A PROPOS</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="https://www.who.int/fr/emergencies/diseases/novel-coronavirus-2019?gclid=Cj0KCQjw7Nj5BRCZARIsABwxDKIppgz-bY8j0mvJ2af-wCbpWZuS4VFz-5z8qNXOwWAcD84MGTKOTgEaAt8hEALw_wcB">ACTUALITES</a>
-  </li>
-   <li class="nav-item">
-    <a class="nav-link" href="notifs.php">NOTIFICATIONS</a>
-  </li
-  <li class="nav-item">
-    <a class="nav-link" href="contact.php">FORMATIONS</a>
-  </li>
-   <li class="nav-item">
-    <a class="nav-link" href="#">CONTACTS</a>
-  </li
-  <li class="nav-item">
-    <a class="nav-link disabled" href="connexin.php" tabindex="-1" aria-disabled="true">CONNEXION</a>
-  </li>
-    <li class="nav-item">
-    <a class="nav-link" href="connexin.php">DECONNEXION</a>
-  </li
-</ul>
-</section>
- <section class = "pic"> <marquee bgcolor="blue">
-<img src="ESSA.PNG" height="150" width="250"/>
-<img src="CAMES.PNG" height="150" width="250"/>
-<img src="ANAQ.JPG" height="150" width="250"/>
-<img src="ESSA1.JPG" height="150" width="250"/>
-<img src="ESSA2.JPG" height="150" width="250"/>
-<img src="ESSA4.JPG" height="150" width="250"/>
-<img src="ESSA5.JPG" height="150" width="250"/>
-<img src="ESSA6.JPG" height="150" width="250"/>
-<img src="ESSA7.JPG" height="150" width="250"/>
-<img src="ESSA8.JPG" height="150" width="250"/>
-<img src="ESSA9.JPG" height="150" width="250"/>
-<img src="ESSA10.JPG" height="150" width="250"/>
-<img src="ESSA11.JPG" height="150" width="250"/>
-<img src="ESSA12.JPG" height="150" width="250"/>
-<img src="ESSA13.JPG" height="150" width="250"/>
-<img src="ESSA14.PNG" height="150" width="250"/>
-<img src="ESSA15.PNG" height="150" width="250"/>
-<img src="ESSA16.PNG" height="150" width="250"/>
-<img src="ESSA17.JPG" height="150" width="250"/>
-<img src="ESSA18.JPG" height="150" width="250"/>
-<img src="ESSA19.JPG" height="150" width="250"/>
-</marquee></section>
-<section class="ff">
-  <marquee direction="up" > <a href="notifs.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">REGARDER LES NOTIFICATIONS</a></marquee>
- </section>
+
+<head>
+    <title>Plateforme ESSA</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="essa.css" />
+</head>
+
+<body>
+
+    <div class="header">
+    <img class="logo" src="ESSA.PNG" height="110" width="180" />
+    BIENVENUE A L'ECOLE SUPERIEURE DES SCIENCES APPLIQUEES E.S.S.A
+    
+    <img class="logo" src="CAMES.PNG" height="110" width="180" />
+    </div>
+    <section class="menu">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link" href="#">A PROPOS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">ACTUALITES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="notifs.php">NOTIFICATIONS</a>
+            </li class="nav-item">
+            <a class="nav-link" href="contact.php">FORMATIONS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">CONTACTS</a>
+            </li <li class="nav-item">
+            <a class="nav-link disabled" href="connexin.php" tabindex="-1" aria-disabled="true">CONNEXION</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="connexin.php">DECONNEXION</a>
+            </li </ul>
+    </section>
+    <section class="pic">
+        <marquee bgcolor="blue">
+            <img src="ESSA.PNG" height="150" width="250" />
+            <img src="CAMES.PNG" height="150" width="250" />
+            <img src="ANAQ.JPG" height="150" width="250" />
+            <img src="ESSA1.JPG" height="150" width="250" />
+            <img src="ESSA2.JPG" height="150" width="250" />
+            <img src="ESSA4.JPG" height="150" width="250" />
+            <img src="ESSA5.JPG" height="150" width="250" />
+            <img src="ESSA6.JPG" height="150" width="250" />
+            <img src="ESSA7.JPG" height="150" width="250" />
+            <img src="ESSA8.JPG" height="150" width="250" />
+            <img src="ESSA9.JPG" height="150" width="250" />
+            <img src="ESSA10.JPG" height="150" width="250" />
+            <img src="ESSA11.JPG" height="150" width="250" />
+            <img src="ESSA12.JPG" height="150" width="250" />
+            <img src="ESSA13.JPG" height="150" width="250" />
+            <img src="ESSA14.PNG" height="150" width="250" />
+            <img src="ESSA15.PNG" height="150" width="250" />
+            <img src="ESSA16.PNG" height="150" width="250" />
+            <img src="ESSA17.JPG" height="150" width="250" />
+            <img src="ESSA18.JPG" height="150" width="250" />
+            <img src="ESSA19.JPG" height="150" width="250" />
+        </marquee>
+    </section>
+    <section class="ff">
+        <marquee direction="up"> <a href="notifs.php" class="btn btn-primary btn-lg active" role="button"
+                aria-pressed="true">REGARDER LES NOTIFICATIONS</a></marquee>
+    </section>
 </body>
 
-	</html>
-				
+</html>
